7e8f98558a3196bf0520bd2038fa4aeb65dc6b76
Yashodip Patil <yashodip.patil@avalara.com>
2025-03-25 12:03:09 +0530

    updaeted

--- src/sum.js
+++ src/sum.js
@@ -1,7 +1,7 @@
-function sum(a, b) {
+function sum123(a, b) {
   return a + b;
 }
 
 // Example usage
-const result = sum(5, 3);
+const result = sum123(5, 3);
 console.log("The sum is:", result); // Output: The sum is: 8
